95dbbcc077eb9fef59c128fe8e034916f052f00b
tdxhhw66 <tdxhhw66@gmail.com>
2016-08-22 23:44:48 +0800

    angular2_demo
    angularjs2示例程序

---
+++ angular2-tour-of-heroes/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+
+import { routing } from './app.routing';
+
+import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
+
+import { HeroesComponent } from './heroes.component';
+
+import { HeroService } from './hero.service';
+
+@NgModule({
+  imports:      [
+        BrowserModule,
+        FormsModule,
+        routing
+        ],
+  declarations: [
+        AppComponent,
+        HeroDetailComponent,
+        DashboardComponent
+        ],
+  providers: [
+        HeroService
+        ],
+  bootstrap:    [
+        AppComponent
+        ]
+})
+export class AppModule { }
---
+++ angular2-tour-of-heroes/app/mock-heroes.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.HEROES = [
+    { id: 11, name: 'Mr.Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'Dynama' },
+    { id: 17, name: 'Magma' }
+];
+//# sourceMappingURL=mock-heroes.js.map
